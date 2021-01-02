66157997be1a79687ca8bd4b8eeef48ec496e1fd
Jason Axelson <jason.axelson@gmail.com>
2021-01-02 13:00:00 -1000

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule MakeupLive.MixProject do
       {:phoenix_live_view, "~> 0.15.2"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:makeup_elixir, "~> 0.14.0"},
+      {:makeup_elixir, "~> 0.15.0"},
       {:makeup, "~> 1.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
