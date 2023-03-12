2a9a31c4b1cabcca595a9995f76be1f4920b78c6
Jason Axelson <jason.axelson@gmail.com>
2023-03-12 08:25:30 -1000

    Update deps (and phoenix 1.7.1)

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule MakeupLive.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "1.7.0-rc.2", override: true},
+      {:phoenix, "~> 1.7.0"},
       {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_live_view, "~> 0.18.3"},
       {:phoenix_html, "~> 3.0"},
